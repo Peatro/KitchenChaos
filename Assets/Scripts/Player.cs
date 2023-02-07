326455f2b1add1e10ae1392cf8c4d70f10a74c69
Peatro <PeatroXD@yandex.ru>
2023-02-07 03:32:19 +0300

    Animator, Idle and walking animations added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _playerSpeed = 7.0f;
 
+    private bool _isWalking = false;
+
     private void Update()
     {  
         Vector2 inputVector = new Vector2(0, 0);
@@ -32,7 +34,14 @@ public class Player : MonoBehaviour
         Vector3 moveDirecrion = new Vector3(inputVector.x, 0.0f, inputVector.y);
         transform.position += moveDirecrion * _playerSpeed * Time.deltaTime;
 
+        _isWalking = moveDirecrion != Vector3.zero;
+
         float rotationSpeed = 10.0f;
         transform.forward = Vector3.Slerp(transform.forward, moveDirecrion, Time.deltaTime * rotationSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
